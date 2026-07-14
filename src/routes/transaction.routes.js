2400fe352b53813dcf21c1263f06376609765453
Siddesh Rizwani <siddeshrizwani@gmail.com>
2026-07-14 23:46:51 +0530

    created the initial funds transfer route

--- src/routes/transaction.routes.js
+++ src/routes/transaction.routes.js
@@ -1,9 +1,20 @@
 const express = require("express");
 const router = express.Router();
 const authMiddleware = require("../middlewares/auth.middleware.js");
-const { createTransaction } = require("../controllers/transaction.controller.js");
+const { createTransaction, createInitialFundsTransaction } = require("../controllers/transaction.controller.js");
 
 // POST /api/transactions
-router.post("/", authMiddleware, createTransaction);
+router.post("/", authMiddleware.authMiddleware, createTransaction);
+
+/**
+ * POST /api/transactions/system/initial-funds
+ * Only the system user can transfer initial funds from the system account.
+ */
+router.post(
+    "/system/initial-funds",
+    authMiddleware.authMiddleware,
+    authMiddleware.authSystemUserMiddleware,
+    createInitialFundsTransaction
+);
 
 module.exports = router;
